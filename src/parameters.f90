4713d6c4e6c00f855af1ae97f608587d24aca8b0
Spencer Alexander <spencer.r.alexander@gmail.com>
2014-03-03 12:32:50 -0700

    typical parameters updated

--- src/parameters.f90
+++ src/parameters.f90
@@ -2,21 +2,29 @@ module parameters
     implicit none
 
     !! Problem Size
-    integer, parameter :: nnx = 50
-    integer, parameter :: nny = 50
+    !! NOTE: Pressure solver only works for hx==hy
+    integer, parameter :: nnx = 100
+    integer, parameter :: nny = 100
     real, parameter :: lx = 1.0
     real, parameter :: ly = 1.0
-    real, parameter :: h = lx/nnx
+    real, parameter :: hx = lx/nnx
+    real, parameter :: hy = ly/nny
+    real, parameter :: hm = min(hx,hy)
 
     !! Flow Parameters
     real, parameter :: uvel = 4.0
+    real, parameter :: dpg  = 2.00 ! works well for 10 turbines
 
     !! Time Step and TF
     real, parameter :: cfl = 0.5
-    real, parameter :: dt = h/uvel*cfl
-    real, parameter :: tf = 1.0
+    real, parameter :: dt = hm/(3*uvel)*cfl
+    real, parameter :: tf = 4.0
     integer, parameter :: nts = tf/dt
     integer, parameter :: niter = 50
+
+    !! Actuator disk setup
+    integer, parameter :: ndisks = 20
+    character (len=*), parameter :: adisk_fname = './adisk.txt'
 
 end module
 
