72e2cc4a78662f4b83e4da379f30a3ea2f44eebb
Spencer Alexander <spencer.r.alexander@gmail.com>
2014-03-05 21:09:22 -0700

    adding a few new parameters and cleaning up

--- src/parameters.f90
+++ src/parameters.f90
@@ -20,11 +20,22 @@ module parameters
     real, parameter :: dt = hm/(3*uvel)*cfl
     real, parameter :: tf = 4.0
     integer, parameter :: nts = tf/dt
+
+    !! Pressure solver
     integer, parameter :: niter = 50
+
+    !! Saving the files
+    integer, parameter :: write_freq = 10
+    character(*), parameter :: write_dir = './data/' ! make sure this exists
 
     !! Actuator disk setup
     integer, parameter :: ndisks = 20
-    character (len=*), parameter :: adisk_fname = './adisk.txt'
+    character (len=*), parameter :: adisk_fname = './adisk.txt' ! make sure this exists
+    integer, parameter :: r = nnx/32
+    real, parameter :: alpha = 0.5
+
+    !! Usefull stuff
+    real, parameter :: pi=3.14159
 
 end module
 
