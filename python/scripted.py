7056b30aebff499da8b9b5edad075d3b3d7d9347
Spencer Alexander <spencer.r.alexander@gmail.com>
2014-03-13 08:36:57 -0600

    putting final touches on run

--- python/scripted.py
+++ python/scripted.py
@@ -8,9 +8,10 @@
 import os
 import random
 import string
+import re
 
 from multiprocessing import Pool
-from numpy import np
+import numpy as np
 
 ## NDISKS: The number of actuator disks to use. This should match the parameter in the cfd executable
 ## NTHREADS: The number of jobs to run simultaneously
@@ -20,11 +21,17 @@ from numpy import np
 NDISKS = 20
 NTHREADS = 4
 NGENERATIONS = 5
-EXECUTABLE = './cfd'
-RUNPREFIX = './'
+EXECUTABLE = './src/cfd'
+RUNPREFIX = './run'
 
-def run(A):
-    # A: actuator locations
+def execute(A):
+    """EXECUTE: Take the disk locations, and run the CFD simulator
+   
+    Inputs:
+          A: Actuator disk layout
+    Outputs:
+          power: The power produced by the layout
+    """
 
     # make up a string, and set up absolute paths
     token = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
@@ -40,22 +47,54 @@ def run(A):
         for a in A:
             f.write('%f %f' % a)
 
-    
-    # Pertubate the actuator locations
-    return A
+    # change into the run directory
+    os.chdir(runpath)
+
+    # run the executable
+    results = os.popen(exepath).read()
+
+    # Parse the results for the power
+    regex = re.compile('Total Power Produced: ([\d.-]*)')
+    r = regex.search(results)
+    power = r.groups()[0]
+    power = float(power)
+
+    # return the results
+    return power
 
 def pertubate(A,dA):
+    """PERTUBATE: Take a layout A, and pertubate it with magnitude dA
+
+    Inputs:
+        A: Initial layout
+        dA: Pertubation magnitude
+    Outputs:
+        A: Perturbed layout
+    """
+
     for i in range(0,NDISKS):
+        # pertubate the disks
         A[i][0] = A[i][0]+ dA*random.random()
         A[i][1] = A[i][1]+ dA*random.random()
+
+        # make sure the disks are located between 0 and 1
+        A[i][0] = min(1,max(0,A[i][0]))
+        A[i][1] = min(1,max(0,A[i][1]))
 
     # make sure things are between 0 and 1
     return A
 
 
 
-def run():
+def start():
+    """START: This is where the magic happens
+    create a random layout, run a generation with random pertubations
+    and take the best of the generation to create a new generation
+    """
+
     random.seed()
+
+    pool = Pool(NTHREADS)
 
     # create the initial A
     A = []
@@ -71,9 +110,9 @@ def run():
         for _ in range(0,NTHREADS):
             dA = 2^(-igen)
             generation.append(pertubate(A,dA))
-        result = pool.map(run,generation)
+        result = pool.map(execute,generation)
 
         # take the best generation, and make it A.
 
 if __name__ == '__main__':
-    run()
+    start()
